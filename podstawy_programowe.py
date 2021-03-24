6ca42e6255e5414caae161b0866a7fea4e4486f3
Piggey <49120531+Piggey@users.noreply.github.com>
2021-03-24 19:23:38 +0100

    fixed formatting

--- podstawy_programowe.py
+++ podstawy_programowe.py
@@ -4,24 +4,24 @@ from json import load
 from sys import platform
 try:
     from os import startfile
-except ImportError as e:
+except ImportError as e: # no startfile when on linux
     pass
 
-
-# get the input
-## Sample input:
-# I. 1. 1) 2) 3) 5) 2. 1) 2) 3) 4) 7) 3. 1) 2) 3) 4) 4. 1) 4) 5) 7) 8) 5. 2) 4) 5) 6)
-# II. 4. 2), 6. 8) 9)
-# III. 1. 1) 3) 4)
 def getLines():
+    # get the input
+    ## Sample input:
+    # I. 1. 1) 2) 3) 5) 2. 1) 2) 3) 4) 7) 3. 1) 2) 3) 4) 4. 1) 4) 5) 7) 8) 5. 2) 4) 5) 6)
+    # II. 4. 2), 6. 8) 9)
+    # III. 1. 1) 3) 4)
     lastLine = 'line'
     lines = []
     print('Wklej indeksy rozdzialow tutaj: ')
     while True:
         line = input()
-        line = line.replace(',', '') # ignore ','
+        line = line.replace(',', '') # ignore ',' character
 
-        # whitespace buffer, not to sure about this
+        # whitespace buffer, not so sure about this
+        ## works aight :>
         if(not lastLine and not line):
             break
         else:
@@ -31,11 +31,11 @@ def getLines():
 
     return lines
 
-# parse input into dictionary calls
-# edu
-## zakres
-### osiagniecia
 def parseInput(lines):
+    # parse input into dictionary calls
+    # edu
+    ## zakres
+    ### osiagniecia
     calls = [] # list of dictionaries
 
     for line in lines:
@@ -58,23 +58,24 @@ def parseInput(lines):
     
     return calls
 
-
-#create .docx document
 def createDocument(data, lines):
+    ## create .docx document
     # header: wklejony input, Times New Roman, 10
     # edu: Times New Roman, 12, pogrubione
     # zakres: Times New Roman, 12, podkreslenie
     # osiagniecie: Times New Roman, 12, zwykly
 
+    print("tworzenie dokumentu...")
     calls = parseInput(lines)
     d = Document()
     font = d.styles['Normal'].font
     font.name = 'Times New Roman'
     font.size = Pt(10)
 
-    # create header
-    for line in lines:
-        p = d.add_paragraph(line)
+    # # create header
+    # for line in lines:
+    #     if(line): # ignore empty lines
+    #         p = d.add_paragraph(line)
 
     font.size = Pt(12)
     last_edu = ''
@@ -110,6 +111,8 @@ def createDocument(data, lines):
             startfile(filename)
     except PermissionError:
         print("Poprzedni plik jest nadal otwarty! Zamknij go przed uzyciem skryptu.")
+
+# -- main -- #
 
 # load json file
 with open('podstawy_programowe.json', encoding='utf-8') as f:
